d926a9c70151784d1af59e37749a94f3b1b6e20d
Nepalcem <mark.emailtest@gmail.com>
2023-10-25 01:31:36 +0300

    ts 1-4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
-const key = new Key();
+// Ключ (Key): Створіть клас Key. У нього має бути одна приватна властивість signature, яка генерується випадково при створенні об'єкта цього класу (наприклад Math.random()). Також цей клас повинен мати метод getSignature, який повертає значення властивості signature.
 
+// Людина (Person): Створіть клас Person. Конструктор цього класу приймає об'єкт класу Key і зберігає їх у приватному властивості key. Клас Person повинен мати метод getKey, який повертає збережений ключ.
+
+// Дім (House): Створіть абстрактний клас House. Цей клас має дві властивості: door, яка може бути відкрита (true), або закрита (false), і key, яка зберігає об'єкт класу Key. У цьому класі також повинен бути метод comeIn, який додає об'єкт класу Person у масив tenants, якщо door відкрита. Ваш абстрактний клас House також повинен мати абстрактний метод OpenDoor, який приймає об'єкт класу Key.
+
+// Мій будинок (MyHouse): Створіть клас MyHouse, який успадковується від абстрактного класу House. Реалізуйте метод openDoor у цьому класі. Якщо ключ, переданий цьому методу, збігається з ключем, збереженим як key, то двері відчиняються.
+
+// Після реалізації всіх класів створіть об'єкти для кожного класу та спробуйте відтворити сценарій, в якому людина приходить додому.
+
+interface IKey {
+  getSignature(): number;
+}
+class Key implements IKey {
+  private signature: number;
+  constructor() {
+    this.signature = Math.floor(Math.random() * 10) + 1;
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+interface IPerson {
+  getKey(): Key;
+}
+
+class Person implements IPerson {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  private tenants: Person[] = [];
+  protected door = false;
+  constructor(protected key: Key) {}
+  comeIn(person: Person): void {
+    this.tenants.push(person);
+  }
+  abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+      if (key.getSignature() === this.key.getSignature()) {
+          return (this.door = true);
+      } else {
+          return this.door = false
+      }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
